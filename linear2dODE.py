caea123f9494227183be179c0794d613ce5a0787
ericgage-williams <ekg3@williams.edu>
2024-01-16 11:35:07 -0500

    add examples

---
+++ linear2dODE.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.cm import rainbow
+import numpy as np
+from scipy.integrate import solve_ivp
+from scipy.io import loadmat
+import pysindy as ps
+from pysindy.utils import linear_damped_SHO
+from pysindy.utils import cubic_damped_SHO
+from pysindy.utils import linear_3D
+from pysindy.utils import hopf
+from pysindy.utils import lorenz
+
+
+
+# ignore user warnings
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
+np.random.seed(1000)  # Seed for reproducibility
+
+# Integrator keywords for solve_ivp
+integrator_keywords = {}
+integrator_keywords['rtol'] = 1e-12
+integrator_keywords['method'] = 'LSODA'
+integrator_keywords['atol'] = 1e-12
+
+# Generate training data
+
+dt = 0.01
+t_train = np.arange(0, 25, dt)
+t_train_span = (t_train[0], t_train[-1])
+x0_train = [2, 0]
+x_train = solve_ivp(linear_damped_SHO, t_train_span,
+                    x0_train, t_eval=t_train, **integrator_keywords).y.T
+
+# Fit the model
+
+poly_order = 5
+threshold = 0.05
+
+model = ps.SINDy(
+    optimizer=ps.STLSQ(threshold=threshold),
+    feature_library=ps.PolynomialLibrary(degree=poly_order),
+)
+model.fit(x_train, t=dt)
+model.print()
+
+# Simulate and plot the results
+
+x_sim = model.simulate(x0_train, t_train)
+plot_kws = dict(linewidth=2)
+
+fig, axs = plt.subplots(1, 2, figsize=(10, 4))
+axs[0].plot(t_train, x_train[:, 0], "r", label="$x_0$", **plot_kws)
+axs[0].plot(t_train, x_train[:, 1], "b", label="$x_1$", alpha=0.4, **plot_kws)
+axs[0].plot(t_train, x_sim[:, 0], "k--", label="model", **plot_kws)
+axs[0].plot(t_train, x_sim[:, 1], "k--")
+axs[0].legend()
+axs[0].set(xlabel="t", ylabel="$x_k$")
+
+axs[1].plot(x_train[:, 0], x_train[:, 1], "r", label="$x_k$", **plot_kws)
+axs[1].plot(x_sim[:, 0], x_sim[:, 1], "k--", label="model", **plot_kws)
+axs[1].legend()
+axs[1].set(xlabel="$x_1$", ylabel="$x_2$")
+fig.show()
